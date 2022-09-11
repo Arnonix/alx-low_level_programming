209ad945231d35bb6f662fc7fefd664a61eda56e
Arnonix <moses.m.maranga@gmail.com>
2022-09-11 09:49:21 -0700

    task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  *Main - main block
+ *   *description - prints the alphabets in lower case
+ *    *return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while c <= 'z';
+	{
+		putchar (c);
+		c++;
+	}
+	 	putchar ('\n');
+		return (0);
+}
+
